4afec44ceaf21c047fbac69fbfc5864554f03126
Rhys Godfrey <rhys.godfrey@gmail.com>
2012-02-26 16:31:06 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,50 @@
+﻿MSBuild Text Replacement Task
+=======================================
+
+## Introduction ##
+
+A MSBuild task to allow replacement of a string in a file with another value.
+
+## Usage ##
+
+Include the DLL in your MSBuild project
+
+	<UsingTask TaskName="TextReplacementTask" AssemblyFile="..\PATH-TO-DLL\RhysG.MSBuild.Common.dll"/>
+
+Then call the *TextReplacementTask* this takes several parameters described below. To replace all instances of the string *$placeholder$* with
+*Replacement Text*:
+
+	<TextReplacementTask File="..\PATH-TO-FILE\SourceFile.js" OutputFile="..\PATH-TO-FILE\OutputFile.js" StringToReplace="$placeholder$" Value="Replacement Text" />
+
+### Parameters ###
+
+<table>
+	<tr>
+		<th>Parameter</th>
+		<th>Description</th>
+	</tr>
+	<tr>
+		<td>File</td>
+		<td>The path to the source file</td>
+	</tr>
+	<tr>
+		<td>OutputFile</td>
+		<td>The path to the output file</td>
+	</tr>
+	<tr>
+		<td>StringToReplace</td>
+		<td>The text to find in the source file</td>
+	</tr>
+	<tr>
+		<td>Value</td>
+		<td>The text to replace all occurences of <em>StringToReplace</em></td>
+	</tr>
+</table>
+
+## License ##
+
+This library is released under the *FreeBSD License*, see **LICENSE.txt** for more information.
+
+Blog: http://www.rhysgodfrey.co.uk
+
+Twitter: @rhysgodfrey
---
+++ TextReplacementTask.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace RhysG.MSBuild.Common
+{
+    public class TextReplacementTask : ITask
+    {
+        private IBuildEngine _buildEngine;
+        private ITaskHost _taskHost;
+
+        public IBuildEngine BuildEngine
+        {
+            get
+            {
+                return _buildEngine;
+            }
+            set
+            {
+                _buildEngine = value;
+            }
+        }
+
+        [Required]
+        public string OutputFile
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string File
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string StringToReplace
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        public bool Execute()
+        {
+            FileInfo file = new FileInfo(File);
+            FileInfo outputFile = new FileInfo(OutputFile);
+
+            if (file.Exists)
+            {
+                if (outputFile.Exists && outputFile.LastWriteTimeUtc == file.LastWriteTimeUtc)
+                {
+                    return true;
+                }
+
+                string input = System.IO.File.ReadAllText(file.FullName);
+
+                string replaced = input.Replace(StringToReplace, Value);
+
+                System.IO.File.WriteAllText(outputFile.FullName, replaced);
+                outputFile.LastWriteTimeUtc = file.LastWriteTimeUtc;
+
+                FileSecurity security = new FileSecurity();
+                security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.ReadAndExecute, AccessControlType.Allow));
+
+                outputFile.SetAccessControl(security);
+
+                BuildEngine.LogMessageEvent(new BuildMessageEventArgs(String.Format("Packed: {0}", file.Name), String.Empty, "PackJavascriptFileTask", MessageImportance.Normal));
+            }
+
+            return true;
+        }
+
+        public ITaskHost HostObject
+        {
+            get
+            {
+                return _taskHost;
+            }
+            set
+            {
+                _taskHost = value;
+            }
+        }
+    }
+}
